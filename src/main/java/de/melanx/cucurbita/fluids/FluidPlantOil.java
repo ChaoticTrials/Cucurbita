86ec36fe6301d03b2e4df22c925b373bfea19429
MelanX <info@melanx.de>
2020-10-17 20:03:03 +0200

    fixed fluid being invisible

--- src/main/java/de/melanx/cucurbita/fluids/FluidPlantOil.java
+++ src/main/java/de/melanx/cucurbita/fluids/FluidPlantOil.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fluids.ForgeFlowingFluid;
 public class FluidPlantOil {
 
     public static final ResourceLocation OIL_SOURCE = new ResourceLocation(Cucurbita.getInstance().modid, "block/plant_oil");
-    private static final FluidAttributes.Builder ATTRIBUTES = FluidAttributes.builder(OIL_SOURCE, OIL_SOURCE).color(0xC9CB94).viscosity(500);
+    private static final FluidAttributes.Builder ATTRIBUTES = FluidAttributes.builder(OIL_SOURCE, OIL_SOURCE).color(0xFFC9CB94).viscosity(500);
     private static final ForgeFlowingFluid.Properties PROPERTIES = new ForgeFlowingFluid.Properties(() -> ModFluids.PLANT_OIL_SOURCE, () -> ModFluids.PLANT_OIL_FLOWING, ATTRIBUTES)
             .block(() -> ModBlocks.PLANT_OIL)
             .bucket(() -> ModItems.PLANT_OIL_BUCKET);
