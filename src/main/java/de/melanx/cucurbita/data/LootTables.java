c3cdd2b88efe7da80864dc1c4e8f795a36fb3ad2
MelanX <info@melanx.de>
2021-03-01 18:58:01 +0100

    fix not fully hollowed pumpkin being completely hollowed when harvested
    closes #2

--- src/main/java/de/melanx/cucurbita/data/LootTables.java
+++ src/main/java/de/melanx/cucurbita/data/LootTables.java
@@ -1,57 +1,39 @@
 package de.melanx.cucurbita.data;
 
-import com.google.common.collect.ImmutableList;
-import com.mojang.datafixers.util.Pair;
 import de.melanx.cucurbita.Cucurbita;
+import de.melanx.cucurbita.blocks.BlockHollowedPumpkin;
+import de.melanx.cucurbita.core.registration.ModBlocks;
+import io.github.noeppi_noeppi.libx.data.provider.BlockLootProviderBase;
 import net.minecraft.block.Block;
-import net.minecraft.block.FlowingFluidBlock;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.LootTableProvider;
-import net.minecraft.data.loot.BlockLootTables;
 import net.minecraft.loot.*;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraft.loot.conditions.SurvivesExplosion;
+import net.minecraft.loot.functions.CopyBlockState;
+import net.minecraft.state.Property;
 
-import javax.annotation.Nonnull;
-import java.util.List;
-import java.util.Map;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+public class LootTables extends BlockLootProviderBase {
 
-public class LootTables extends LootTableProvider {
     public LootTables(DataGenerator generator) {
-        super(generator);
-    }
-
-    @Nonnull
-    @Override
-    protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> getTables() {
-        return ImmutableList.of(
-                Pair.of(BlockTable::new, LootParameterSets.BLOCK)
-        );
+        super(Cucurbita.getInstance(), generator);
     }
 
     @Override
-    protected void validate(@Nonnull Map<ResourceLocation, LootTable> map, @Nonnull ValidationTracker validationtracker) {
-        map.forEach((name, table) -> LootTableManager.validateLootTable(validationtracker, name, table));
+    protected void setup() {
+        this.customLootTable(ModBlocks.PLANT_OIL);
+        this.copyProperties(ModBlocks.HOLLOWED_PUMPKIN, BlockHollowedPumpkin.CARVING);
     }
 
-    private static class BlockTable extends BlockLootTables {
-        @Override
-        protected void addTables() {
-            ForgeRegistries.BLOCKS.getEntries().stream().map(Map.Entry::getValue).filter(block -> block.getRegistryName().getNamespace().equals(Cucurbita.getInstance().modid) &&
-                    !(block instanceof FlowingFluidBlock))
-                    .collect(Collectors.toList()).forEach(this::registerDropSelfLootTable);
-        }
+    private void copyProperties(Block b, Property<?>... properties) {
+        LootEntry.Builder<?> entry = ItemLootEntry.builder(b);
+        CopyBlockState.Builder func = CopyBlockState.func_227545_a_(b);
 
-        @Nonnull
-        @Override
-        protected Iterable<Block> getKnownBlocks() {
-            Stream<Block> blockStream = ForgeRegistries.BLOCKS.getEntries().stream().map(Map.Entry::getValue).filter(block -> block.getRegistryName().getNamespace().equals(Cucurbita.getInstance().modid));
-            return blockStream::iterator;
+
+        for (Property<?> property : properties) {
+            func = func.func_227552_a_(property);
         }
+        LootPool.Builder pool = LootPool.builder().name("main").rolls(ConstantRange.of(1)).addEntry(entry)
+                .acceptCondition(SurvivesExplosion.builder())
+                .acceptFunction(func);
+        this.customLootTable(b, LootTable.builder().addLootPool(pool));
     }
 }
